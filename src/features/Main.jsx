df04385c6c769d2e047debf1ff000818386cae3b
hoangminh-2409 <buidanghoangminh2409@gmail.com>
2023-10-26 10:51:33 +0700

    component

--- src/features/Main.jsx
+++ src/features/Main.jsx
@@ -1,27 +1,37 @@
-import React from "react";
-import ProductCart from './ProductCart';
-const Main = () => {
-    return(
-        <main className='py-5'>
-            <div className="Container px-4 px-lg-5 mt-5">
-                <div className = 'row gx-4 gx-lg-5 row-cols-2 row-cols-1'>
-                    <div className="col mb-5">
-                        <ProductCart />
-                    </div>
-                    <div className="col mb-5">
-                        <ProductCart />
-                    </div>
-                    <div className="col mb-5">
-                        <ProductCart />
-                    </div>
-                    <div className="col mb-5">
-                        <ProductCart />
-                    </div>
-                    <div className="col mb-5">
-                        <ProductCart />
-                    </div>
+    import React from 'react'
+    import ProductCard from './ProductCard';
 
-                </div>
+    const Main = () => {
+        return (
+            <div className="container px-4 px-lg-5 mt-5">
+        <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div className="col mb-5">
+            <ProductCard />
             </div>
-        </main>    
-)}
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+            <div className="col mb-5">
+            <ProductCard />
+            </div>
+        </div>
+        </div>
+        );
+    }
+
+    export default Main;
